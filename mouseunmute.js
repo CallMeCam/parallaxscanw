415881b9b2f96ec058691ab765a3c39a7c152bf5
CallMeCam <37002723+CallMeCam@users.noreply.github.com>
2020-04-14 23:54:09 -0700

    Update mouseunmute.js

--- mouseunmute.js
+++ mouseunmute.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
-    var backAudio = $('#mediaplayer');
-    var muted = false;
-    
-$('.mute').mouseover(function(){
-        var video = $(this);
-        if (!muted) {
-            video.attr("disabled", "");
-            backAudio.animate({volume: 0}, 1000, function () {
-                muted = true;
-            });
-        }
-        else {
-            video.attr("disabled", "");
-            backAudio.animate({volume: 1}, 1000, function () {
-                muted = false;
-            });
-        }
-    });
+var backAudio = $('#mediaplayer');
+var muted = true;
+backAudio[0].volume = 0;
+
+ $('.mute').hover(function(){
+     var video = $(this);
+     if (muted) {
+         video.attr("disabled", "");
+         backAudio.animate({volume: 1}, 1000, function () {
+             muted = false;
+         });
+     }
+     else {
+         video.attr("disabled", "");
+         backAudio.animate({volume: 0}, 1000, function () {
+             muted = true;
+         });
+     }
+ });
 });
